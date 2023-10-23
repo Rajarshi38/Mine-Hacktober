f8ace086ea131428dfd59c52521b923dd2dc8666
Rajarshi38 <rajarshibiswas38@gmail.com>
2023-10-23 11:30:25 +0530

    created a server and added a get request

--- go/main.go
+++ go/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func main() {
-	var name string = "Rajarshi";
-	fmt.Println(name)
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID string `json:"id"`
+	Item string `json:"item"`
+	Completed bool `json:"completed"`
+}
+
+
+var todos = []todo{
+	{ID : "1",Item : "Clean Room",Completed : false},
+	{ID : "2",Item: "Do homework",Completed : false},
+}
+
+func getTodos(context *gin.Context){
+	context.IndentedJSON(http.StatusOK,todos)
+}
+
+
+func main(){
+	router := gin.Default();
+	router.GET("/todos",getTodos)
+	router.Run("localhost:8080")
 }
